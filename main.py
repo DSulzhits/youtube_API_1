19b5e9e86a57489ca3ee10da2af988166e42d0de
DSulzhits <dsulzhits@gmail.com>
2023-03-05 23:21:57 +0300

    добавлены строки

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from class_channel.class_channel import Channel
 
 def main():
     green_grass = Channel('UCbaKd4mhqd1QvfDp5EaUlHw')
-    # print(green_grass.channel_info)
+    info = green_grass.channel_info
+    print(info)
     # print(green_grass.channel_id)
     # print(green_grass.channel_title)
     # print(green_grass.channel_description)
@@ -11,9 +12,9 @@ def main():
     # print(green_grass.channel_videoCount)
     # print(green_grass.channel_viewCount)
     # green_grass.channel_id = 'Новое название'
-    # green_grass.make_json()
+    green_grass.make_json(green_grass.channel_title)
     vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-    # vdud.make_json()
+    vdud.make_json(vdud.channel_title)
     print(green_grass)
     print(vdud)
     print(green_grass > vdud)
