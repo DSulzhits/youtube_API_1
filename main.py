e91bfef5e84871208301ed73655c3b7cd79c3c57
DSulzhits <dsulzhits@gmail.com>
2023-03-05 21:22:04 +0300

    восстановлен main.py

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
-import os
-import json
-from googleapiclient.discovery import build
+from class_channel.class_channel import Channel
+
+def main():
+    green_grass = Channel('UCbaKd4mhqd1QvfDp5EaUlHw')
+    # print(green_grass.channel_info)
+    # print(green_grass.channel_id)
+    # print(green_grass.channel_title)
+    # print(green_grass.channel_description)
+    # print(green_grass.channel_link)
+    # print(green_grass.channel_subscribers)
+    # print(green_grass.channel_videoCount)
+    # print(green_grass.channel_viewCount)
+    # green_grass.channel_id = 'Новое название'
+    # green_grass.make_json()
+    vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+    # vdud.make_json()
+    print(green_grass)
+    print(vdud)
+    print(green_grass > vdud)
+    print(green_grass < vdud)
+    print(green_grass + vdud)
+    pass
 
 
-class Youtube:
-    """Создаем класс Youtube как основной для обращения к самому ютубу и получения оттуда необходимых данных о канале"""
-    api_key: str = os.getenv('YT_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
 
-    @classmethod
-    def get_channel(cls, channel_id: str):
-        yt_channel = cls.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-        return yt_channel
-
-
-class Channel:
-    """Создаем класс для работы с полученными данными от ютуба"""
-
-    def __init__(self, channel_id: str):
-        """Инициализируем для работы с необходимыми данными, сами данные делаем приватными,
-        чтобы пользователь не мог вносить в них изменения"""
-        self.__id = channel_id
-        self.__info = Youtube.get_channel(channel_id)
-        self.__title = self.__info['items'][0]['snippet']['title']
-        self.__description = self.__info['items'][0]['snippet']['description']
-        self.__link = 'https://www.youtube.com/' + self.__info['items'][0]['snippet']['customUrl']
-        self.__subscribers = int(self.__info['items'][0]['statistics']['subscriberCount'])
-        self.__videoCount = int(self.__info['items'][0]['statistics']['videoCount'])
-        self.__viewCount = int(self.__info['items'][0]['statistics']['viewCount'])
+if __name__ == "__main__":
+    main()
